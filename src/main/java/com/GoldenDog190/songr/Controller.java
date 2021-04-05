c87ee76ae06777a3ec6cb838f0ecb03cb5486ae1
Amelia Waggoner <wildginger@wavecable.com>
2021-04-05 16:43:26 -0700

    Did tasks 1, 2, 3, & 4

---
+++ src/main/java/com/GoldenDog190/songr/Controller.java
@@ -0,0 +1,24 @@
+package com.GoldenDog190.songr;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/hello-world")
+        public String helloWorldPage(){
+            return "hello-world.html";
+    }
+
+    @GetMapping("/capitalize/{capitalize}")
+        public String capitalizePage(Model m,
+                                     @PathVariable String capitalize){
+        String capitalizeWord = new String("Hello");
+        System.out.println("capitalize = " + capitalize);
+        System.out.println(capitalizeWord.toUpperCase());
+        m.addAttribute("capitalize", capitalize.toUpperCase());
+        return "capitalize.html";
+    }
+}
